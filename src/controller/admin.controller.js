2ab41588cfc31689d21dbc7bc3e17ce004f50aac
yash rai <yash2154rai@gmail.com>
2024-06-19 17:09:49 +0530

    chage in logout function in controller

--- src/controller/admin.controller.js
+++ src/controller/admin.controller.js
@@ -63,8 +63,13 @@ const login = async (req, res) => {
 
 // Admin logout
 const logout = (req, res) => {
-  res.clearCookie("jwt");
-  return res.status(200).json({ message: "Logout successful" });
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 export { createAdmin, login, logout };
