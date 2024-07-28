8ee4d39a93778c51697e764738c017ab508c2bf7
yash rai <yash2154rai@gmail.com>
2024-07-28 22:19:34 +0530

    change in generate token

--- src/util/genratetoken.js
+++ src/util/genratetoken.js
@@ -16,10 +16,11 @@ const generateTokenAndSetCookie = (userId, res) => {
     res.cookie("jwtkey", token, {
       maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in milliseconds
       httpOnly: true, // Prevent XSS attacks
-      sameSite: isProduction ? "strict" : "lax", // CSRF attacks protection
+      sameSite: isProduction ? "None" : "Lax", // CSRF attacks protection
       secure: isProduction, // Use secure cookies in production
     });
 
+    // Optionally, you can log the token for debugging purposes (commented out for security reasons)
     // console.log("Token generated and set as cookie:", token);
   } catch (error) {
     console.error("Error generating token and setting cookie:", error);
